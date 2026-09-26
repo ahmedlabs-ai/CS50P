bb7591c567d72ba443ea2f5718990ac8c0d6088d
ahmedlabs-ai <ahmed1200.cs@gmail.com>
2026-09-26 09:38:18 +0600

    Remove accidental nested Problem Set duplicates

---
+++ Problem Set 5/Test plates/test_plates.py
@@ -0,0 +1,13 @@
+from plates import is_valid
+
+def test_valid():
+    assert is_valid('CS50') == True
+    assert is_valid('CS05') == False
+    assert is_valid('CS50P') == False
+    assert is_valid('PI3.14') == False
+    assert is_valid('H') == False
+    assert is_valid('OUTATIME') == False
+    assert is_valid("CS01") == False
+    assert is_valid("CS10") == True
+    assert is_valid("50CS") == False
+    assert is_valid("C50") == False
--- Problem Set 5/Test twttr/test_twttr.py
+++
@@ -1,7 +0,0 @@
-from twttr import shorten
-
-def test_shorten():
-    assert shorten("twitter") == "twttr"
-    assert shorten("AEIOU") == ""
-    assert shorten("CS50") == "CS50"
-    assert shorten("hello") == "hll"
